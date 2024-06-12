33eee6c40155cc24d8656b87d33fa2957f7f8119
Briankimany <briankimani328@gmail.com>
2024-06-12 13:52:38 +0300

    My changes to app.py before pull

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 
-# A very simple Flask Hello World app for you to get started with...
+from server import app
 
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
-
+if __name__ == "__main__":
+    app.run()
